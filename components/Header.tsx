36fd9d64474b72691a10baf0d732a2604a357795
MinuraWije <minurawije@gmail.com>
2025-02-28 17:27:23 +0530

    todo delete done

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import {HeaderView,HeaderTitle,HeaderButton,colors} from "../styles/appStyles";
 import {Entypo} from '@expo/vector-icons'
 
-const Header = () => {
+const Header = ({handleClearTodos}) => {
     return (
         <HeaderView>
             <HeaderTitle>Todos</HeaderTitle>
-            <HeaderButton>
+            <HeaderButton
+                onPress={handleClearTodos}
+            >
                 <Entypo name='trash' size={25} color={colors.tertiary}/>
             </HeaderButton>
         </HeaderView>
--- components/Home.tsx
+++ components/Home.tsx
@@ -1,10 +1,41 @@
-import React from "react";
-import {Text} from 'react-native';
+import React,{useState} from "react";
 import Header from './Header';
+import ListItems from "./ListItems";
+import InputModal from "./InputModal";
 
 const Home = () => {
+
+    const initialTodos = [{
+        title:"Get Started",
+        date: "Fri,  today",
+        key:"1"
+    },{
+        title:"Have Lunch",
+        date: "Fri,  today",
+        key:"2"
+    },{
+        title:"Workout",
+        date: "Fri,  today",
+        key:"3"
+    },]
+
+    const [todos,setTodos] = useState(initialTodos)
+
+    const handleClearTodos = () =>{
+        setTodos([]);
+    }
+
+
     return (
-        <Header/>
+        <>
+            <Header handleClearTodos={handleClearTodos}/>
+            <ListItems
+                todos={todos}
+                setTodos={setTodos}
+            />
+            <InputModal/>
+        </>
+
     );
 }
 
